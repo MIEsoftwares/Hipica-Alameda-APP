8c20f63c2cb7ae08f403f924db745591b3d37d3d
mock <mockismymain@gmail.com>
2024-05-26 00:29:52 -0300

    refactor: login agora está funcionando e instalação de mais algumas dependências

--- database/SupabaseConfig.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = "https://wxmasywkvxmgjlhqgjfp.supabase.co"
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind4bWFzeXdrdnhtZ2psaHFnamZwIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcxNTg5MTQ3NywiZXhwIjoyMDMxNDY3NDc3fQ.0GonEP_vuqVz3uUMKwSd76b-cslHITnPcN4oV_oS6JU"
-const supabase = createClient(supabaseUrl, supabaseKey)
-
-export default supabase
