bf8c377f98a1854255d97483ab4414da92e50cc9
MIEsoftwares <mie.softwares.contact@gmail.com>
2024-06-03 11:31:09 -0300

    fix: Retirada chave do import

--- database/auth/register.js
+++ database/auth/register.js
@@ -1,4 +1,4 @@
-import { supabase } from "../SupabaseConfig";
+import supabase from "../SupabaseConfig";
 
 export async function signUpWithEmail(email, nome, senha, cpf, telefone, proprietario_de_cavalo, nome_do_cavalo) {
     const {
