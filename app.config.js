18c54ed867ff3235686c55fc653ff4b4e1fb376b
mockjk <mockismymain@gmail.com>
2024-12-03 21:43:34 -0300

    :sparkles: feat: update libs and setup project

---
+++ app.config.js
@@ -0,0 +1,40 @@
+export default {
+    expo: {
+      name: "Hipica-Alameda-App",
+      slug: "Hipica-Alameda-App",
+      version: "1.0.0",
+      orientation: "portrait",
+      icon: "./src/assets/images/Logo2.png",
+      userInterfaceStyle: "light",
+      splash: {
+        image: "./src/assets/images/Logo1-splash.png",
+        resizeMode: "contain",
+        backgroundColor: "#ffffff",
+      },
+      ios: {
+        supportsTablet: true,
+      },
+      android: {
+        adaptiveIcon: {
+          foregroundImage: "./assets/adaptive-icon.png",
+          backgroundColor: "#ffffff",
+        },
+        package: "com.mockjk.HipicaAlamedaApp",
+      },
+      extra: {
+      eas: {
+        "projectId": "8d66abac-6977-4c54-929c-1d97d2667dc2"
+      },
+    },
+      plugins: [
+        [
+          "expo-build-properties",
+          {
+            android: {
+              minSdkVersion: 24,
+            },
+          },
+        ],
+      ],
+    },
+  };
